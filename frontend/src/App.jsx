5a88a043c97e28eba6fb7de1cc9da4965fe27a4f
rediet-g-tech <redietgebermichael@gmail.com>
2026-01-12 21:15:12 +0300

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -60,7 +60,6 @@ import Signup from "./components/Signup/Signup";
 import Howitworks from "./pages/HowItWorks/Howitworks";
 import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
 import ResetPassword from "./components/ForgotPassword/ResetPassword";
-import Howitworks from "./pages/Howitworks/Howitworks";
 
 
 function App() {
