cd796145a5523fbca7c9b1efdceb6976e866bbad
rediet-g-tech <redietgebermichael@gmail.com>
2026-01-13 19:01:42 +0300

    update

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import app from "./app.js";
+// import dotenv from "dotenv";
+// dotenv.config();
 import dotenv from "dotenv";
 dotenv.config();
+
 const PORT =  4000;
 app.listen(PORT, () =>
   console.log(`Server running at http://localhost:${PORT}`)
